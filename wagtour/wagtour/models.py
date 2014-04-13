f44f76a77253732198901c072f7e56ac45085d73
Yan Aung <yan.pye.aung@gmail.com>
2014-04-13 16:33:17 -0700

    Add models for blog post and blog index pages

---
+++ wagtour/wagtour/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel  ,MultiFieldPanel,InlinePanel, PageChooserPanel
+from modelcluster.fields import ParentalKey  
+
+class BlogPage(Page):
+    body = RichTextField()
+    date = models.DateField("Post date")
+    search_name = "Blog Page"
+
+    indexed_fields = ('body', )
+
+BlogPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('date'),
+    FieldPanel('body', classname="full"),
+]
+
+class LinkFields(models.Model):
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    panels = [
+        PageChooserPanel('link_page'),
+    ]
+
+    class Meta:
+        abstract = True
+
+class RelatedLink(LinkFields):
+    title = models.CharField(max_length=255, help_text="Link title")
+    panels = [
+        FieldPanel('title'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class BlogIndexPageRelatedLink(Orderable, RelatedLink):
+    page = ParentalKey('wagtour.BlogIndexPage', related_name='related_links')
+
+class BlogIndexPage(Page):
+    intro = models.CharField(max_length=256)
+    indexed_fields = ('body', )
+    search_name = "Blog Index Page"
+
+BlogIndexPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('intro', classname="full"),
+    InlinePanel(BlogIndexPage, 'related_links', label="Related links"),
+]
